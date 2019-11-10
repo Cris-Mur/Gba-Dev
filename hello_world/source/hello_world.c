8b27f27c6a27283a19cacb5f063c1efdf4380cfc
Cris-Mur <yomy98@gmail.com>
2019-11-10 08:47:17 -0500

    [add first project]

---
+++ hello_world/source/hello_world.c
@@ -0,0 +1,29 @@
+#include <gba_console.h>
+#include <gba_video.h>
+#include <gba_interrupt.h>
+#include <gba_systemcalls.h>
+#include <gba_input.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	char *name = "Cristian Murcia.";
+	// the vblank interrupt must be enabled for VBlankIntrWait() to work
+	// since the default dispatcher handles the bios flags no vblank handler
+	// is required
+	irqInit();
+	irqEnable(IRQ_VBLANK);
+
+	consoledemoInit();
+
+	printf("My name is: %s", name);
+	
+	
+	// ansi escape sequence to set print co-ordinates
+	// /x1b[line;columnH
+	iprintf("\x1b[2;3HHello World!\n");
+	
+	while (1) {
+		VBlankIntrWait();
+	}
+}
