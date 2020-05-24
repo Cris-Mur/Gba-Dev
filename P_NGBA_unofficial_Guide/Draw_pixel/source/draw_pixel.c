c585d6d6a551702248aa566f06b8089e10e7f3e9
Cris-Mur <yomy98@gmail.com>
2020-05-23 19:10:31 -0500

    [draw a pixel]

---
+++ P_NGBA_unofficial_Guide/Draw_pixel/source/draw_pixel.c
@@ -0,0 +1,20 @@
+#include <gba.h>
+
+//////////////////////////////////////////////////
+// Programming The Game Boy Advance
+// Chapter 4: Starting With The Basics
+// DrawPixel Project
+// main.c source code file
+//////////////////////////////////////////////////
+int main(void)
+{
+	*(unsigned int*)0x04000000 = 0x0403;
+
+    	((unsigned short*)0x06000000)[120+80*240] = 0x001F;
+    	((unsigned short*)0x06000000)[136+80*240] = 0x03E0;
+    	((unsigned short*)0x06000000)[120+96*240] = 0x7C00;
+
+    	while(1);
+
+    	return 0;
+}
